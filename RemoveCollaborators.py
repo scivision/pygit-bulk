20679cbda714a96b35900e2d7c947ed9e7dee62a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-09-11 10:42:29 -0400

    init

--- RemoveCollaborators.py
+++ RemoveCollaborators.py
@@ -22,26 +22,26 @@ import webbrowser
 
 def main():
     p = ArgumentParser(description='Set all collaborator permission to "read"')
-    p.add_argument('user', help='GitHub username / organizations')
-    p.add_argument('oauth', help='Oauth filename')
-    p.add_argument('pattern', help='modify repos with name starting with this string')
-    p.add_argument('--omit', help='dont consider these admins', nargs='+')
+    p.add_argument("user", help="GitHub username / organizations")
+    p.add_argument("oauth", help="Oauth filename")
+    p.add_argument("pattern", help="modify repos with name starting with this string")
+    p.add_argument("--omit", help="dont consider these admins", nargs="+")
     P = p.parse_args()
 
-# %% authentication
+    # %% authentication
     sess = gb.github_session(P.oauth)
     gb.check_api_limit(sess)
-# %% get user / organization handle
+    # %% get user / organization handle
     userorg = gb.user_or_org(sess, P.user)
-# %% prepare to loop over repos
+    # %% prepare to loop over repos
     repos = gb.get_repos(userorg)
 
     to_modify = [repo for repo in repos if repo.name.startswith(P.pattern)]
     if not to_modify:
-        raise SystemExit(f'no repos to modify under {P.user}/{P.pattern}')
+        raise SystemExit(f"no repos to modify under {P.user}/{P.pattern}")
 
-    print('\ntype affirmative to remove all collaborators from\n', '\n'.join([repo.full_name for repo in to_modify]))
-    modify = input() == 'affirmative'
+    print("\ntype affirmative to remove all collaborators from\n", "\n".join([repo.full_name for repo in to_modify]))
+    modify = input() == "affirmative"
 
     for repo in to_modify:
         collabs = repo.get_collaborators()
@@ -50,16 +50,16 @@ def main():
         if not admins:
             continue
 
-        print('admins', repo.full_name, ' '.join(admins))
+        print("admins", repo.full_name, " ".join(admins))
         if modify:
             if repo.archived:
-                logging.error(f'could not remove collabs from archived {repo.full_name}')
-                webbrowser.open_new_tab('https://github.com/' + repo.full_name + '/settings')
+                logging.error(f"could not remove collabs from archived {repo.full_name}")
+                webbrowser.open_new_tab("https://github.com/" + repo.full_name + "/settings")
                 continue
 
             for admin in admins:
                 repo.remove_from_collaborators(admin)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- SetPrivate.py
+++ SetPrivate.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 """
-Archive GitHub repos for a user/organization with repo names matching pattern
+Set Private GitHub repos for a user/organization with repo names matching pattern
 
-Requires GitHub Oauth login with sufficient permissions "repo:public_repo" or "repo" for private repos.
-(admin:org Oauth does not work)
+Requires GitHub Oauth login with sufficient permissions "repo:public_repo" to make public private
+"repo" permission needed to see private.
+"admin:org" Oauth does not work.
 It's suggested you create an Oauth key for this, and then disable/delete this key permissions when done
 to avoid a security issue.
 To do this, including for Organizations, go to your
@@ -15,41 +16,44 @@ if you get error
     github.GithubException.UnknownObjectException: 404 {'message': 'Not Found',
     'documentation_url': 'https://developer.github.com/v3/repos/#edit'}
 
-that typically means your Oauth key doesn't have adequte permissions.
+that typically means your Oauth key doesn't have adequate permissions.
+
+example:
+
+    python SetPrivate.py myorg ~/.ssh/Oauth foo-
 """
 from argparse import ArgumentParser
 import gitutils.github_base as gb
 
 
 def main():
-    p = ArgumentParser(description="Set GitHub repos to Archive matching pattern")
-    p.add_argument("user", help="GitHub username / organizations")
+    p = ArgumentParser(description="Set GitHub repos to Private matching pattern")
+    p.add_argument("userorgname", help="GitHub username / organizations")
     p.add_argument("oauth", help="Oauth filename")
-    p.add_argument("pattern", help="archive repos with name starting with this string")
+    p.add_argument("pattern", help="make private repos with name starting with this string")
     P = p.parse_args()
 
     # %% authentication
     sess = gb.github_session(P.oauth)
     gb.check_api_limit(sess)
     # %% get user / organization handle
-    userorg = gb.user_or_org(sess, P.user)
+    userorg = gb.user_or_org(sess, P.userorgname)
     # %% prepare to loop over repos
     repos = gb.get_repos(userorg)
     if not repos:
         raise SystemExit(f"no repos for {P.user}")
 
-    to_archive = [repo for repo in repos if repo.name.startswith(P.pattern) and not repo.archived]
-    if not to_archive:
-        raise SystemExit(f"no repos left to archive under {P.user}/{P.pattern}")
+    to_act = [repo for repo in repos if repo.name.startswith(P.pattern) and not repo.private]
+    if not to_act:
+        raise SystemExit(f"no repos left matching {P.user}/{P.pattern}")
 
-    print("NOTE: presently, you can only UNarchive through the website manually.")
-    print("\ntype affirmative to ARCHIVE (make read-only)", "\n".join([repo.full_name for repo in to_archive]))
+    print("\ntype affirmative to make PRIVATE", "\n".join([repo.full_name for repo in to_act]))
     if input() != "affirmative":
         raise SystemExit("Aborted")
 
-    for repo in to_archive:
-        repo.edit(archived=True)
-        print("archived", repo.full_name)
+    for repo in to_act:
+        repo.edit(private=True)
+        print("private:", repo.full_name)
 
 
 if __name__ == "__main__":

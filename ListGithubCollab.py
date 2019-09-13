5a077d186fc28a68e46e5cce3c3e512d9e12b3fc
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-09-13 14:48:03 -0400

    List: print missing usernames optionall

--- ListGithubCollab.py
+++ ListGithubCollab.py
@@ -2,31 +2,49 @@
 """
 Lists collaborators for GitHub repo or repos starting with pattern.
 
+Optionally with -xls use a spreadsheet filename and column to find Github usernames
+that haven't accepted their invite yet (assuming you sent it previously).
+
+    python ListGithubCollab.py ~/.ssh/oauth myowg -stem foo- -xls users.xlsx C
+
 oauth token must have "read:org" (and "repo" for private repos) permissions
 https://developer.github.com/v3/repos/#oauth-scope-requirements
 """
 from argparse import ArgumentParser
+import pandas
+from pathlib import Path
+import itertools
+
 from gitedu.get import get_collabs
 from gitutils.github_base import check_api_limit, connect_github
 
 
-def main():
+def main(P):
+    op, sess = connect_github(P.oauth, P.orgname)
+    if not check_api_limit(sess):
+        raise RuntimeError("GitHub API limit exceeded")
+
+    return get_collabs(op, sess, P.stem, P.regex)
+
+
+if __name__ == "__main__":
     p = ArgumentParser()
     p.add_argument("oauth", help="Oauth filename")
     p.add_argument("orgname", help="Github organization name")
     p.add_argument("-stem", help="reponame starts with")
     p.add_argument("-regex", help="regex pattern of reponame")
+    p.add_argument("-xls", help="spreadsheet filename and column to find missing usernames (who isn't signed up)", nargs=2)
     P = p.parse_args()
 
-    op, sess = connect_github(P.oauth, P.orgname)
-    if not check_api_limit(sess):
-        raise RuntimeError("GitHub API limit exceeded")
+    collabs = main(P)
 
-    collabs = get_collabs(op, sess, P.stem, P.regex)
+    for k in sorted(collabs.keys()):
+        print(k, collabs[k])
 
-    for k, v in collabs.items():
-        print(k, v)
+    if P.xls:
+        xlsfn = Path(P.xls[0]).expanduser()
+        required = pandas.read_excel(xlsfn, usecols=P.xls[1]).squeeze().tolist()
+        present = list(itertools.chain.from_iterable(collabs.values()))
 
-
-if __name__ == "__main__":
-    main()
+        missing = set(required).difference(present)
+        print("Missing:\n", missing)

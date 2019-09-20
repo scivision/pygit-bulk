69bd915fdf894959a75b04f10d59e74b1ea8f3d8
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-09-20 14:05:13 -0400

    allow fast --empty mode to just find empty repos

--- PlotCommits.py
+++ PlotCommits.py
@@ -21,54 +21,70 @@ As a first pass, just shows total LoC changed. Future: plot commit vs. time.
 """
 from argparse import ArgumentParser
 import typing
-from matplotlib.pyplot import figure, show
+
+try:
+    from matplotlib.pyplot import figure, show
+except ImportError:
+    figure = show = None
 
 import gitutils.github_base as gb
 import github.GithubException
 
 
-def main() -> typing.List[str]:
-    p = ArgumentParser()
-    p.add_argument("user", help="GitHub username / organizations")
-    p.add_argument("oauth", help="Oauth filename")
-    p.add_argument("pattern", help="repos with name starting with this string")
-    P = p.parse_args()
-
+def main(user: str, oauth: str, pattern: str, only_empty: bool) -> typing.List[str]:
     # %% authentication
-    sess = gb.github_session(P.oauth)
+    sess = gb.github_session(oauth)
     gb.check_api_limit(sess)
     # %% get user / organization handle
-    userorg = gb.user_or_org(sess, P.user)
+    userorg = gb.user_or_org(sess, user)
     # %% prepare to loop over repos
     repos = gb.get_repos(userorg)
 
-    to_act = (repo for repo in repos if repo.name.startswith(P.pattern))
+    to_act = (repo for repo in repos if repo.name.startswith(pattern))
 
     empty: typing.List[str] = []
     for repo in to_act:
+        print(f"examining {repo.name}", end="\r")
         authors: typing.Dict[str, int] = {}
         try:
             for commit in repo.get_commits():
+                if only_empty:
+                    break
+                if not commit.stats:  # GitHub API bug?
+                    continue
                 if commit.stats.total == 0:
+                    continue
+                if not commit.author:  # GitHub API bug?
                     continue
                 if commit.author.login in authors:
                     authors[commit.author.login] += commit.stats.total
                 else:
                     authors[commit.author.login] = commit.stats.total
-            ax = figure().gca()
-            ax.scatter(authors.keys(), authors.values())
-            ax.get_yaxis().get_major_formatter().set_useOffset(False)
-            ax.set_ylabel("total LoC changed")
-            ax.set_yscale("log")
-            ax.set_title(repo.name)
+            if not only_empty:
+                ax = figure().gca()
+                ax.scatter(authors.keys(), authors.values())
+                ax.get_yaxis().get_major_formatter().set_useOffset(False)
+                ax.set_ylabel("total LoC changed")
+                ax.set_yscale("log")
+                ax.set_title(repo.name)
         except github.GithubException as exc:
             if "empty" in exc.data["message"]:
-                empty += repo.name
+                empty.append(repo.name)
+    print()  # flush stdout \r
 
     return sorted(empty)
 
 
 if __name__ == "__main__":
-    empty = main()
+    p = ArgumentParser()
+    p.add_argument("userorg", help="GitHub username / organizations")
+    p.add_argument("oauth", help="Oauth filename")
+    p.add_argument("pattern", help="repos with name starting with this string")
+    p.add_argument("--empty", help="don't plot, just print out empty repos", action="store_true")
+    P = p.parse_args()
+
+    only_empty = P.empty or show is None
+
+    empty = main(P.userorg, P.oauth, P.pattern, only_empty)
     print("\n".join(empty))
     show()

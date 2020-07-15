6fb581a340546ea2f1d2544afc61414734b7da33
Michael Hirsch <10931741+scivision@users.noreply.github.com>
2020-07-15 10:37:14 -0400

    remove unreachable code

--- pygithubutils/base.py
+++ pygithubutils/base.py
@@ -41,7 +41,6 @@ def check_api_limit(g: github.Github = None) -> bool:
 
     if api_remaining == 0:
         raise ConnectionRefusedError(f"GitHub rate limit exceeded: {api_remaining} / {api_max}. Try again after {treset} UTC.")
-        ok = False
     # it's not elif !
     if api_remaining < 10:
         logging.warning(ResourceWarning(f"approaching GitHub API limit, {api_remaining} / {api_max} remaining until {treset} UTC."))
